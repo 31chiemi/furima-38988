111b84e2857a2de649c251959165115e1981f304
31chiemi <31chiemi@gmail.com>
2023-03-21 14:09:28 +0900

    deviseに関するビューファイルの配置
    他CSSファイルも配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :birthday, presence: true
+  with_options presence: true, format: { with:/\A[ァ-ヶー－]+\z/} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end

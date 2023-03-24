3af25a433e8be4717114d6aae7053d62d7d8a9f5
31chiemi <31chiemi@gmail.com>
2023-03-24 12:36:39 +0900

    バリデーション実施
    numericalityでカテゴリー５項目は指定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,15 @@ class Item < ApplicationRecord
   belongs_to :charge
   belongs_to :prefecture
   belongs_to :term
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :charge_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :term_id, numericality: { other_than: 1 } 
 end

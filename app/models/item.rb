51fdab36339fdbf771bf1c6f6788527bf878b73f
31chiemi <31chiemi@gmail.com>
2023-03-25 16:48:28 +0900

    正規表現の文字チェック削除・only_integer: trueオプション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,5 @@ class Item < ApplicationRecord
   validates :charge_id, numericality: { other_than: 1 } 
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :term_id, numericality: { other_than: 1 } 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-  format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end

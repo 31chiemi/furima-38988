227cc21c1b8cfc8edcc7686e4ca328c935b3b593
31chiemi <31chiemi@gmail.com>
2023-03-27 18:07:45 +0900

    set_itemを適用し記述省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,8 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
-    unless current_user.id ==  @item.user_id
+    unless current_user.id ==  set_item.user_id
       redirect_to root_path
     end
   end

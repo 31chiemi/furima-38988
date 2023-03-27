dae8cc2363301035b653be4486525d7fa2db1f76
31chiemi <31chiemi@gmail.com>
2023-03-27 17:18:24 +0900

    edit・updateアクション定義
    合わせてbeforeメソッドでparam取得をまとめる(set_item)
    move_to_indexで出品者以外をトップページへ戻す

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show, :update,]
+  before_action :move_to_index, only: [:edit]
   # 出品時にログインしていない場合はログイン画面に推移する
+  # edit show updateの実行前にset_itemを実行する
+  # 出品者以外がログインしている際はmove_to_indexを実行する
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,12 +24,33 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id) 
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :content, :price, :category_id, :condition_id, :charge_id, :prefecture_id, :term_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id ==  @item.user_id
+      redirect_to root_path
+    end
   end
 end

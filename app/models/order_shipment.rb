da164a44814e79cca63da9b634dd8d8175f8f334
31chiemi <31chiemi@gmail.com>
2023-03-30 11:59:12 +0900

    バリデーション修正

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -1,17 +1,21 @@
 class OrderShipment
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phonenumber, :item_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phonenumber, :item_id , :user_id
   with_options presence: true do
+    validates :city
+    validates :block
+    validates :phonenumber
     validates :item_id
+    validates :user_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 } 
 
   def save
     # 購入情報を保存し、変数orderに代入する
     order = Order.create(item_id: item_id, user_id: user_id)
     # 住所を保存する
     # order_idには、変数orderのidと指定する
-    Address.create(postcode: postcode, prefecture: prefecture_id, city: city, block: block, building: building, phonenumber: phonenumber, order_id: order.id)
+    Shipment.create(postcode: postcode, prefecture: prefecture_id, city: city, block: block, building: building, phonenumber: phonenumber, order_id: order.id)
   end
 end

368da0369fc0717a49bc0ef172f13620e5f4c90d
31chiemi <31chiemi@gmail.com>
2023-04-01 16:21:33 +0900

    ストロングパラメーターに値追加pay_item定義
    order_paramsに商品の値段・トークン情報が保存され、サーバーサイドへ受け渡し可能とする記述。およびpay_itemにてトークン情報および環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order_shipment = OrderShipment.new(order_params)
     if @order_shipment.valid?
+      pay_item
       @order_shipment.save
       redirect_to root_path
     else
@@ -18,6 +19,15 @@ class OrdersController < ApplicationController
 
   def order_params
     @item = Item.find(params[:item_id])
-    params.require(:order_shipment).permit(:postcode, :prefecture_id, :city, :block, :building, :phonenumber).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_shipment).permit(:postcode, :prefecture_id, :city, :block, :building, :phonenumber).merge(user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end

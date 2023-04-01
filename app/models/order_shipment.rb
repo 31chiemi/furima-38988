00e64d3945c2b6fe27a4590b75b036cda6f4c31e
31chiemi <31chiemi@gmail.com>
2023-04-01 16:22:02 +0900

    トークン・値段の属性追加、バリデーション設定

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -1,6 +1,6 @@
 class OrderShipment
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phonenumber, :item_id , :user_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phonenumber, :item_id , :user_id, :token, :price
   with_options presence: true do
     validates :postcode,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
@@ -8,6 +8,8 @@ class OrderShipment
     validates :phonenumber,  format: {with: /\A\d{10,11}\z/ }
     validates :item_id
     validates :user_id
+    validates :token
+    validates :price
   end
     validates :prefecture_id, numericality: {other_than: 1 } 
   

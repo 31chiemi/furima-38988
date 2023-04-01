36af03a2d9a54f9ecdd87fe7aedd138b574e6e9f
31chiemi <31chiemi@gmail.com>
2023-04-01 12:41:05 +0900

    コントローラー修正（エラーコード出力のため）
    ifで要件分岐させvalid？を記述しエラーコードをブラウザ上に出力、renderを用いて入力値が消えないようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,23 @@
 class OrdersController < ApplicationController
   def index
-    @order_shipment = OrderShipment.new
+    @item = Item.find(params[:item_id])
+    @order_shipment  = OrderShipment.new
   end
   
   def create
     @order_shipment = OrderShipment.new(order_params)
-    @order_shipment.save
+    if @order_shipment.valid?
+      @order_shipment.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
   
   private
 
   def order_params
-    params.require(:order_shipment).permit(:postcode, :prefecture, :city, :block, :building, :phonenumber).merge(user_id: current_user.id, item_id: item.id)
+    @item = Item.find(params[:item_id])
+    params.require(:order_shipment).permit(:postcode, :prefecture_id, :city, :block, :building, :phonenumber).merge(user_id: current_user.id, item_id: @item.id)
   end
-
 end

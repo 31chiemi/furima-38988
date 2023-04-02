3b66743826e84ca1d6b184bc5fcb5db0d46ae03c
31chiemi <31chiemi@gmail.com>
2023-04-02 17:22:44 +0900

    if user_signed_in?の削除・リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_item
   before_action :move_to_item_index, only: [:index]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_shipment  = OrderShipment.new
   end
   
@@ -20,29 +20,28 @@ class OrdersController < ApplicationController
   
   private
 
-  def order_params
+  def set_item
     @item = Item.find(params[:item_id])
-    params.require(:order_shipment).permit(:postcode, :prefecture_id, :city, :block, :building, :phonenumber).merge(user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price)
+  end
+
+  def order_params
+    params.require(:order_shipment).permit(:postcode, :prefecture_id, :city, :block, :building, :phonenumber).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                # 通貨の種類（日本円）
     )
   end
   
   def move_to_item_index
-    @item = Item.find(params[:item_id])
-    if user_signed_in?
       if current_user.id == @item.user_id
-      redirect_to root_path
+        redirect_to root_path
       elsif @item.order != nil && current_user.id != @item.user_id
-      redirect_to root_path
+        redirect_to root_path
       end
-    end 
-  end
-
+    end
 end
